35c95f52f7472d34c80b440007b22e25425c072a
Dražen Karamatić <dkaramatic@questel.com>
2024-04-15 07:02:04 +0200

    Score tests

--- src/LiveOdds/Score.cs
+++ src/LiveOdds/Score.cs
@@ -1,9 +1,15 @@
-﻿namespace LiveOdds
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace LiveOdds
 {
     public class Score
     {
         internal Score() { }
 
         public Score(uint home, uint away) { }
+
+        public uint HomeScore { get; private set; }
+
+        public uint AwayScore { get; private set; }
     }
 }
---
+++ src/LiveOddsTests/ScoreTests.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LiveOdds.Tests
+{
+    [TestClass()]
+    public class ScoreTests
+    {
+        [TestMethod()]
+        public void ScoreTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
